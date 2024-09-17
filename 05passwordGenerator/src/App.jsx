07dcbe7d67d457ab3a2f20916e0d7165743eb7b0
Kumar_Anshuman <kanshuman6484@gmail.com>
2024-09-17 23:45:42 +0530

    password generator

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -26,60 +26,62 @@ function App() {
 
   return (
     <>
-      <div
-        className="w-full max-w-md mx-auto flex-wrap shadow-md rounded-lg px-4 py-3 my-8"
-        style={{ color: '#2CF129', backgroundColor: '#001823' }}
-      >
-        <h1 className="text-center text-2xl mb-4 my-2">
-          Random Password Generator
-        </h1>
-        <div className="flex shadow rounded-lg overflow-hidden mb-4">
-          <input
-            type="text"
-            value={password}
-            className="outline-none w-full py-1 px-3 text-slate-800"
-            placeholder="password"
-            readOnly
-          />
-          <button className="outline-none bg-blue-700 text-white px-3 py=0.5 shrink-0">
-            Copy
-          </button>
-        </div>
-        <div className="flex text-sm gap-x-4">
-          <div className="flex items-center gap-x-1">
+      <div className="w-full h-screen flex justify-center items-center">
+        <div
+          className="w-full max-w-md mx-auto flex-wrap shadow-md rounded-lg px-4 py-3 my-8"
+          style={{ color: '#2CF129', backgroundColor: '#001823' }}
+        >
+          <h1 className="text-center text-2xl mb-4 my-2">
+            Random Password Generator
+          </h1>
+          <div className="flex shadow rounded-lg overflow-hidden mb-4">
             <input
-              type="range"
-              min={8}
-              max={30}
-              value={length}
-              className="cursor-pointer"
-              onChange={(e) => {
-                setLength(e.target.value)
-              }}
+              type="text"
+              value={password}
+              className="outline-none w-full py-1 px-3 text-slate-800"
+              placeholder="password"
+              readOnly
             />
-            <label>Length : {length}</label>
+            <button className="outline-none bg-blue-700 text-white px-3 py=0.5 shrink-0">
+              Copy
+            </button>
           </div>
-          <div className="flex items-center gap-x-1">
-            <input
-              type="checkbox"
-              defaultChecked={num}
-              id="numInput"
-              onChange={() => {
-                setNum((prev) => !prev)
-              }}
-            />
-            <label htmlFor="numInput">Numbers</label>
-          </div>
-          <div className="flex items-center gap-x-1">
-            <input
-              type="checkbox"
-              defaultChecked={char}
-              id="charInput"
-              onChange={() => {
-                setChar((prev) => !prev)
-              }}
-            />
-            <label htmlFor="charInput">Characters</label>
+          <div className="flex text-sm gap-x-4">
+            <div className="flex items-center gap-x-1">
+              <input
+                type="range"
+                min={8}
+                max={30}
+                value={length}
+                className="cursor-pointer"
+                onChange={(e) => {
+                  setLength(e.target.value)
+                }}
+              />
+              <label>Length : {length}</label>
+            </div>
+            <div className="flex items-center gap-x-1">
+              <input
+                type="checkbox"
+                defaultChecked={num}
+                id="numInput"
+                onChange={() => {
+                  setNum((prev) => !prev)
+                }}
+              />
+              <label htmlFor="numInput">Numbers</label>
+            </div>
+            <div className="flex items-center gap-x-1">
+              <input
+                type="checkbox"
+                defaultChecked={char}
+                id="charInput"
+                onChange={() => {
+                  setChar((prev) => !prev)
+                }}
+              />
+              <label htmlFor="charInput">Characters</label>
+            </div>
           </div>
         </div>
       </div>
