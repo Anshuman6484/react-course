9c9e1067d9c11371aea1102c1551e3dc69e4db9b
Kumar_Anshuman <kanshuman6484@gmail.com>
2024-09-18 16:19:12 +0530

    password generator

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,10 +1,14 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
+import './App.css'
 
 function App() {
   const [length, setLength] = useState(8)
   const [num, setNum] = useState(false)
   const [char, setChar] = useState(false)
   const [password, setPassword] = useState('')
+
+  // useRef hook
+  const passwordRef = useRef(null)
 
   const passwordGenerator = useCallback(() => {
     let pass = ''
@@ -13,12 +17,22 @@ function App() {
     if (char) str += '~!@#$%^&*_-+=?'
 
     for (let i = 1; i <= length; i++) {
-      let ch = Math.floor(Math.random() * str.length + 1)
+      let ch = Math.floor(Math.random() * str.length)
       pass += str.charAt(ch)
     }
 
     setPassword(pass)
-  }, [length, num, char, setPassword])
+  }, [length, num, char])
+
+  const copyPasswordtoClipboard = useCallback(() => {
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+
+    // to select a specific range with min length = 8
+    // let n = Math.floor(Math.random() * length + 8)
+    // passwordRef.current?.setSelectionRange(0, n)
+    // window.navigator.clipboard.writeText(password)
+  }, [password])
 
   useEffect(() => {
     passwordGenerator()
@@ -28,21 +42,27 @@ function App() {
     <>
       <div className="w-full h-screen flex justify-center items-center">
         <div
-          className="w-full max-w-md mx-auto flex-wrap shadow-md rounded-lg px-4 py-3 my-8"
-          style={{ color: '#2CF129', backgroundColor: '#001823' }}
+          id="outer-box"
+          className="w-full max-w-md mx-auto flex-wrap shadow-lg shadow-slate-800 rounded-lg px-4 py-3 my-8"
+          // style={{ color: '#2CF129', backgroundColor: '#001823' }}
         >
           <h1 className="text-center text-2xl mb-4 my-2">
             Random Password Generator
           </h1>
-          <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <div className="flex shadow-md shadow-slate-800 rounded-lg overflow-hidden mb-4">
             <input
               type="text"
               value={password}
               className="outline-none w-full py-1 px-3 text-slate-800"
               placeholder="password"
               readOnly
+              ref={passwordRef}
             />
-            <button className="outline-none bg-blue-700 text-white px-3 py=0.5 shrink-0">
+            <button
+              onClick={copyPasswordtoClipboard}
+              id="btn-copy"
+              className="outline-none text-white px-3 py=0.5 shrink-0"
+            >
               Copy
             </button>
           </div>
