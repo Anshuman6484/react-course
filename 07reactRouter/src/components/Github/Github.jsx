028c52aaf2b7b565e9ee988831389e58b321f9fb
Kumar_Anshuman <kanshuman6484@gmail.com>
2025-01-11 19:50:59 +0530

    reactRouter

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -9,7 +9,7 @@ function Github() {
   }, [])
   return (
     <div className="text-center m-4 p-4 bg-orange-700 text-white text-2xl">
-      Your github followers : {data.followers}
+      Github followers : {data.followers}
       <img src={data.avatar_url} alt="img" width={200} />
     </div>
   )
