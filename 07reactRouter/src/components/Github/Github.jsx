e0c6d56fec88f17d8525002501af106b820df5c7
Kumar_Anshuman <kanshuman6484@gmail.com>
2025-01-11 19:43:44 +0530

    reactRouter

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+
+function Github() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch('https://api.github.com/users/Anshuman6484')
+      .then((res) => res.json())
+      .then((data) => setData(data))
+  }, [])
+  return (
+    <div className="text-center m-4 p-4 bg-orange-700 text-white text-2xl">
+      Your github followers : {data.followers}
+      <img src={data.avatar_url} alt="img" width={200} />
+    </div>
+  )
+}
+
+export default Github
