242fb98bb4eb6109c483d1744ce81f47a77e52e6
Kumar_Anshuman <kanshuman6484@gmail.com>
2025-01-12 19:49:43 +0530

    react-router

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-  const [data, setData] = useState([])
-  useEffect(() => {
-    fetch('https://api.github.com/users/Anshuman6484')
-      .then((res) => res.json())
-      .then((data) => setData(data))
-  }, [])
+  const data = useLoaderData()
+  //   const [data, setData] = useState([])
+  //   useEffect(() => {
+  //     fetch('https://api.github.com/users/Anshuman6484')
+  //       .then((res) => res.json())
+  //       .then((data) => setData(data))
+  //   }, [])
   return (
     <div className="text-center m-4 p-4 bg-orange-700 text-white text-2xl">
       Github followers : {data.followers}
@@ -16,3 +18,8 @@ function Github() {
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+  const res = await fetch('https://api.github.com/users/Anshuman6484')
+  return res.json()
+}
